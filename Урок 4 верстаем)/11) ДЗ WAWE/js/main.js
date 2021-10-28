07616a21c1f24d0df5ad14d8cb31c7de0549913a
eenurike <eenurike@gmail.com>
2021-10-28 17:12:16 +0600

    адаптив(

--- Урок 4 верстаем)/11) ДЗ WAWE/js/main.js
+++ Урок 4 верстаем)/11) ДЗ WAWE/js/main.js
@@ -5,5 +5,16 @@ $(function(){
     arrows: false
   });
 
+  $('.burger__menu').click('on', function() {
+    $('ul.menu').toggleClass('burger__active');
+    $('.burger__menu span').toggleClass('span__active');
+  });
+
+  // $('.container').click('on', function() {
+  //   $('ul.menu').removeClass('burger__active');
+  // });
+
+  
+
   var mixer = mixitup('.gallery__content--inner');
 });
