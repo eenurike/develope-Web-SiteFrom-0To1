78c9394a6781861b31997e6aaa3e9c42edfc9211
eenurike <eenurike@gmail.com>
2021-11-05 17:06:27 +0600

    Плавный скрол

--- Урок 4 верстаем)/10) Адаптив Portfolio, slider/js/main.js
+++ Урок 4 верстаем)/10) Адаптив Portfolio, slider/js/main.js
@@ -1,5 +1,21 @@
 $(function(){
 
+  /* Плавный скрол */
+  $(".menu, .toTop").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href');
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+		var	top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
+  /* Прилипающая навигация */
   $(document).on('scroll', function() {
     let top = $(this).scrollTop();
 
@@ -11,40 +27,40 @@ $(function(){
 
   });
 
-    $('.slider__inner').slick({
-        dots: true,
-        slidesToShow: 1,
-        prevArrow: '<button type="button" class="slick-prev"><img src="images/logos/arrow-left.svg"></button>',
-        /* Путь прописываем как будто мы в html, так как это путь прямо
-        генерируется в html */
-        nextArrow: '<button type="button" class="slick-next"><img src="images/logos/arrow-right.svg"></button>',
-        /* Так мы записываем наши кастомные кнопки */
-        responsive: [
-            {
-              breakpoint: 450,
-              settings: {
-                arrows: false
-              }
-            }]
-    });
+  $('.slider__inner').slick({
+      dots: true,
+      slidesToShow: 1,
+      prevArrow: '<button type="button" class="slick-prev"><img src="images/logos/arrow-left.svg"></button>',
+      /* Путь прописываем как будто мы в html, так как это путь прямо
+      генерируется в html */
+      nextArrow: '<button type="button" class="slick-next"><img src="images/logos/arrow-right.svg"></button>',
+      /* Так мы записываем наши кастомные кнопки */
+      responsive: [
+          {
+            breakpoint: 450,
+            settings: {
+              arrows: false
+            }
+          }]
+  });
 
-
-    $('.menu__btn').on('click', function() {
-        // $('.menu__list').toggleClass('active__list');
-        /* И теперь нужно добавить классы изменения цвета при клике, но каждому задавать
-        это не гуманно, общее у выезжающего меню и бургер меню и логотипа это родитель */
-        $('.header__top-inner').toggleClass('header__top-inner--active');
-    });
-    
-    
-    var mixer = mixitup('.portfolio__content',{
-        animation: {
-            enable: false
-        }
-        /* Убрали анимацию, у плагина на сайте все опции прописанны */
-    });
-    /* Тут пишем наш родительский блок картинок, который
-    будет отфильтровываться.
-    И mixitup плагин лучше писать в самом конце, потомучто если его написать
-    в самом начале после него ничего не работает */
+  /* Добавили menu a, чтоб при клике на ссылку менюха закрывалась */
+  $('.menu__btn, .menu a').on('click', function() {
+      // $('.menu__list').toggleClass('active__list');
+      /* И теперь нужно добавить классы изменения цвета при клике, но каждому задавать
+      это не гуманно, общее у выезжающего меню и бургер меню и логотипа это родитель */
+      $('.header__top-inner').toggleClass('header__top-inner--active');
+  });
+  
+  
+  var mixer = mixitup('.portfolio__content',{
+      animation: {
+          enable: false
+      }
+      /* Убрали анимацию, у плагина на сайте все опции прописанны */
+  });
+  /* Тут пишем наш родительский блок картинок, который
+  будет отфильтровываться.
+  И mixitup плагин лучше писать в самом конце, потомучто если его написать
+  в самом начале после него ничего не работает */
 });
--- Урок 4 верстаем)/11) ДЗ WAWE/js/main.js
+++ Урок 4 верстаем)/11) ДЗ WAWE/js/main.js
@@ -1,4 +1,12 @@
 $(function(){
+
+  $(".menu a, .toTop a").on("click", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href');
+		var	top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
   $(document).on('scroll', function() {
     let top = $(this).scrollTop();
 
@@ -10,8 +18,10 @@ $(function(){
 
     if(top > 60) {
       $('.header__top').addClass('scroll');
+      $('.header').addClass('header__padding');
     } else {
       $('.header__top').removeClass('scroll');
+      $('.header').removeClass('header__padding');
     }
 
   });
@@ -22,7 +32,7 @@ $(function(){
     arrows: false
   });
 
-  $('.burger__menu').click('on', function() {
+  $('.burger__menu, .menu a').click('on', function() {
     $('ul.menu').toggleClass('burger__active');
     $('.burger__menu span').toggleClass('span__active');
   });
