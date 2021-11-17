f05cba5f61642d23a910eb8428ad04b752d4ffd1
eenurike <eenurike@gmail.com>
2021-11-17 22:15:11 +0600

    gulp №2

--- Урок 4 верстаем)/14) Gulp/start_gulp/gulpfile.js
+++ Урок 4 верстаем)/14) Gulp/start_gulp/gulpfile.js
@@ -1,4 +1,11 @@
-/* Эти все плагины смотри на сайте https://www.npmjs.com/ */
+/* Эти все плагины смотри на сайте https://www.npmjs.com/ 
+Если при начале нового проекта, после установки npm i, у тебя не запускается gulp, то нужно
+gulp установить глобально npm i gulp -g
+И если выдает ошибку "gulp : Невозможно загрузить файл C:\Users\Админ\AppData\Roaming\npm\gulp.ps1"
+то нужно дать доступ в powershell через запуск администратора, там пишешь Set-ExecutionPolicy unrestricted,
+и даешь разрешение */
+
+/* Все плагины скачаивай с nom i --save-dev bla */
 
 const {src, dest, watch} = require('gulp');
 /* Деструктурировали наш gulp */
@@ -7,6 +14,8 @@ const scss = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 /* Обьединение файлов, сжатие файлов в min и переименование файлов */
 const autoprefixer = require('gulp-autoprefixer');
+/* Минификация для js файлов */
+const uglify = require('gulp-uglify');
 
 function styles() {
   return src('app/scss/style.scss')
@@ -32,6 +41,25 @@ function styles() {
   Так же создаем переменную dest из gulp */
 }
 
+/* Так же и со скриптами */
+function scripts() {
+  return src([
+    'node_modules/jquery/dist/jquery.js',
+    /* Зачем путь указывать к минифицированной версии, если мы будем сжимать файл */
+    'app/js/main.js'
+    /* Файлов js может быть много, так что записываем в квадратных скобках */
+  ])
+  .pipe(concat('main.min.js'))
+  /* Чтоб они все обьединились в общий файл, но они не будут минифицированы, так как
+  для js нет такого compressed как для css. Нам нужно скачать другой плагин gulp-uglify */
+  .pipe(uglify())
+  /* Теперь файл минифицирован */
+  .pipe(dest('app/js'));
+  /* И будет помещен в папку js, для пробы скачаем какойто файл js, ну для работы нам
+  ведь понадобится jquery, можно скачивать как обычно с официалки, ну а зачем мы изучаем 
+  gulp, если и в gulp можно установить jquery >npm i --save-dev jquery */
+}
+
 function watching() {
   watch(['app/scss/**/*.scss'], styles);
   /* Так же создаем переменную dest из gulp. 
@@ -44,4 +72,5 @@ function watching() {
 
 exports.styles = styles;
 /* И в терминале прописываешь gulp styles, и в папке css создается файл css */
+exports.scripts = scripts;
 exports.watching = watching;
